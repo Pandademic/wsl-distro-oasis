34f7634b3077c3009e57437821e684a636dee279
atha <74566464+Pandademic@users.noreply.github.com>
2022-07-28 18:06:53 -0400

    make the install script work

--- install.ps1
+++ install.ps1
@@ -1,1 +1,15 @@
 echo "installing oasis-linux to wsl"
+
+echo "cloning repo... (step 1 of 3)"
+
+git clone https://github.com/Pandademic/wsl-distro-oasis
+
+echo "creating install directory './oasis-linux-install'... (step 2 of 3)"
+
+mkdir oasis-linux-install
+
+echo "import oasis with 'wsl --import' ...(step 3 of 3)"
+
+wsl --import Oasis oasis-linux-install wsl-distro-oasis/rootfs.tar.gz
+
+echo "finished! Hope you enjoy Oasis linux!"
